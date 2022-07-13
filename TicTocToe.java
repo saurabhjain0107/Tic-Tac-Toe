51e84ebd560b475dcd9bb2a0f19f6d1d1efada24
saurabh jain <luckyjain0107@gmai.com>
2022-07-13 17:34:22 +0530

    Create Board

--- TicTocToe.java
+++ TicTocToe.java
@@ -1,7 +1,29 @@
 package com.bridgelabz;
 
 public class TicTocToe {
+
+    static char board[]=new char[10];
+
+    static void createBoard(char[]board){
+        for(int i=1;i<board.length;i++){
+            board[i]= '_';
+        }
+    }
+    static void showBoard(char[]board){
+        for(int i=1;i<board.length;i++){
+            System.out.print(board[i]);
+            if(i%3==0){
+                System.out.println();
+            }
+        }
+    }
+
+
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game !");
+        createBoard(board);
+        showBoard(board);
     }
 }
